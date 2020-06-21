f73a2a3abd90b9e598bfcca255b213b4dbfc2acf
iremys <iremys@gmail.com>
2020-06-21 13:50:07 +0900

    app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var { Sequelize, Model, DataTypes } = require('sequelize');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -20,8 +21,74 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+/* sequelize */
+const sequelize = new Sequelize({
+  host: "localhost",
+  port: 3307,
+  database: 'node',
+  username: 'root',
+  password: '000000',
+  dialect: 'mysql',
+  pool: { max: 10 }
+});
+
+class SeqBoard extends Model {}
+SeqBoard.init({
+  title: {
+    type: DataTypes.STRING(255),
+    allowNull: false,
+  },
+  content: {
+    type: DataTypes.TEXT()
+  },
+  writer: {
+    type: DataTypes.STRING(50)
+  },
+  rnum: {
+    type: DataTypes.INTEGER(11),
+    defaultValue: 0,
+  }
+}, {
+  sequelize,
+  modelName: 'SeqBoard',
+  timestamps: true,
+  charset: 'utf8',
+  tableName: 'seq-boards'
+});
+SeqBoard.sync({force: false});  
+// true옵션을 주면 기존 테이블을 삭제하고 재생성하므로 절대 실서버에서는 사용하면 안된다.
+
+app.get("/create", async (req, res, next) => {
+  try {
+    const result = await SeqBoard.create({
+      title: "아버지를 아버지라...",
+      writer: '홍길동',
+      content: '아버지를 아버지라...형을 형이라...',
+      rnum: 0
+    });
+    res.json(result);
+  }
+  catch(e) {
+    next(e);
+  }
+})
+
+
+
+sequelize
+.authenticate()
+.then(() => {
+  console.log("mysql에 접속되었습니다.");
+})
+.catch((e) => {
+  throw new Error(e);
+});
+
+
+/* router */
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/board', boardRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
