cd12b946881844fa591e95fb876600280c2e662c
iremys <iremys@gmail.com>
2020-06-21 13:03:50 +0900

    board router 생성

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var boardRouter = require('./routes/board');
 
 var app = express();
 
